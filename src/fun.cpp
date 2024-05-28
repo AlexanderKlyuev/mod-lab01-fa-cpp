a1be96671d9d6165141d712e78bebdd227ac4c71
AlexanderKlyuev <161642862+AlexanderKlyuev@users.noreply.github.com>
2024-05-28 14:37:53 +0300

    Add files via upload

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,111 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
+#include <cstring>
+#include <cmath>
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasDigit = false;
+
+    for (size_t i = 0; i < std::strlen(str); ++i) {
+        if (std::isspace(str[i])) {
+            if (inWord && !hasDigit) {
+                ++count;
+            }
+            inWord = false;
+            hasDigit = false;
+        }
+        else {
+            if (!inWord) {
+                inWord = true;
+                hasDigit = false;
+            }
+            if (std::isdigit(str[i])) {
+                hasDigit = true;
+            }
+        }
+    }
+
+    if (inWord && !hasDigit) {
+        ++count;
+    }
+
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool validWord = false;
+
+    for (size_t i = 0; i < std::strlen(str); ++i) {
+        if (std::isspace(str[i])) {
+            if (inWord && validWord) {
+                ++count;
+            }
+            inWord = false;
+            validWord = false;
+        }
+        else {
+            if (!inWord) {
+                if (std::isupper(str[i])) {
+                    inWord = true;
+                    validWord = true;
+                }
+                else {
+                    inWord = false;
+                    validWord = false;
+                }
+            }
+            else {
+                if (!std::islower(str[i])) {
+                    validWord = false;
+                }
+            }
+        }
+    }
+
+    if (inWord && validWord) {
+        ++count;
+    }
+
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int wordCount = 0;
+    unsigned int totalLength = 0;
+    bool inWord = false;
+    unsigned int currentWordLength = 0;
+
+    for (size_t i = 0; i < std::strlen(str); ++i) {
+        if (std::isspace(str[i])) {
+            if (inWord) {
+                wordCount++;
+                totalLength += currentWordLength;
+                inWord = false;
+                currentWordLength = 0;
+            }
+        }
+        else {
+            if (!inWord) {
+                inWord = true;
+            }
+            currentWordLength++;
+        }
+    }
+
+    if (inWord) {
+        wordCount++;
+        totalLength += currentWordLength;
+    }
+
+    if (wordCount == 0) {
+        return 0;
+    }
+
+    double averageLength = static_cast<double>(totalLength) / wordCount;
+    return static_cast<unsigned int>(std::round(averageLength));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
+    const char* testStr1 = "Hello world! This is a test 123 string.";
+    const char* testStr2 = "Alice has a Cat Bob's friend.";
+    const char* testStr3 = "Average word length calculation.";
+
+    unsigned int countNoDigits = faStr1(testStr1);
+    std::cout << "Количество слов без цифр: " << countNoDigits << std::endl;
+
+    unsigned int countCapitalWords = faStr2(testStr2);
+    std::cout << "Количество слов, начинающихся с заглавной латинской буквы и содержащих только строчные буквы: " << countCapitalWords << std::endl;
+
+    unsigned int averageWordLength = faStr3(testStr3);
+    std::cout << "Средняя длина слова: " << averageWordLength << std::endl;
+
     return 0;
 }
